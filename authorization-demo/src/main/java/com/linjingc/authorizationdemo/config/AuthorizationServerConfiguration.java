b9f34db9e9a7085b1310894252b7872a05586a7a
summerCat <583188551@qq.com>
2019-07-25 01:13:08 +0800

    初始化提交 待测试

--- authorization-demo/src/main/java/com/linjingc/authorizationdemo/config/AuthorizationServerConfiguration.java
+++ authorization-demo/src/main/java/com/linjingc/authorizationdemo/config/AuthorizationServerConfiguration.java
@@ -5,21 +5,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.jwt.crypto.sign.MacSigner;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.approval.ApprovalStore;
-import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 /**
  * 授权服务
@@ -36,7 +35,7 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
     private AuthenticationManager authenticationManager;
 
     @Autowired
-    RedisConnectionFactory redisConnectionFactory;
+    RedisTemplate redisTemplate;
 
 
     /**
@@ -80,6 +79,7 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
     /**
      * 配置（授权服务器安全配置器安全）
      * 访问tokenkey时需要经过认证
+     *
      * @param security
      * @throws Exception
      */
@@ -88,13 +88,14 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
         //访问tokenkey时需要经过认证
         security.tokenKeyAccess("isAuthenticated()");
         //security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
-             // .allowFormAuthenticationForClients(); //允许表单认证  这段代码在授权码模式下会导致无法根据code　获取token　
+        // .allowFormAuthenticationForClients(); //允许表单认证  这段代码在授权码模式下会导致无法根据code　获取token　
 
     }
 
 
     /**
      * TokenStore
+     *
      * @return
      */
     @Bean
@@ -104,13 +105,46 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 
     /**
      * 生成JTW token
+     *
      * @return
      */
     @Bean
-    public JwtAccessTokenConverter jwtAccessTokenConverter(){
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
         converter.setSigningKey("linjingc");
+        String salt = BCrypt.gensalt(); // 实时生成加密的salt
+        // redisTemplate.opsForValue().set("token:" + user.getUsername(), salt, 3600, TimeUnit.SECONDS);
+        converter.setSigner(new MacSigner(salt));
         return converter;
     }
 
+
+    //@Bean
+    //public TokenEnhancer tokenEnhancer() {
+    //    return new CustomTokenEnhancer();
+    //}
+    //
+    //public class CustomTokenEnhancer implements TokenEnhancer {
+    //    @Override
+    //    public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+    //        Map<String, Object> additionalInfo = new HashMap<>();
+    //        additionalInfo.put("organization", authentication.getName());
+    //        ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
+    //        return accessToken;
+    //    }
+//}
+
+/**
+ * String salt = BCrypt.gensalt(); // 实时生成加密的salt
+ * redisTemplate.opsForValue().set("token:"+user.getUsername(), salt, 3600, TimeUnit.SECONDS);
+ * <p>
+ * Algorithm algorithm = Algorithm.HMAC256(salt);
+ * //Date date = new Date(System.currentTimeMillis()+3600*1000);  //设置1小时后过期
+ * <p>
+ * return JWT.create()
+ * .withSubject(user.getUsername())
+ * .withExpiresAt(DateUtils.addDays(new Date(), 1))
+ * .withIssuedAt(new Date())
+ * .sign(algorithm);
+ */
 }
