285bf30d576d7ec02eb415a1b4b8acfa736e5d1b
summerCat <583188551@qq.com>
2019-07-29 18:21:16 +0800

    提交注销后置处理器

---
+++ zuul-demo/src/main/java/com/linjingc/zuuldemo/config/MySsoLogoutHandler.java
@@ -0,0 +1,77 @@
+package com.linjingc.zuuldemo.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+
+/**
+ * 自定义oauth2 注销方法
+ */
+
+@Component
+@Qualifier("mySsoLogoutHandler")
+public class MySsoLogoutHandler extends SecurityContextLogoutHandler implements LogoutHandler {
+
+    @Value("${auth-server}/${auth-server-logout-method}")
+    String logoutUrl;
+
+    @Override
+    public void logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) {
+        //LOGGER.debug("executing MySsoLogoutHandler.logout");
+        Object details = authentication.getDetails();
+        if (details.getClass().isAssignableFrom(OAuth2AuthenticationDetails.class)) {
+
+            String accessToken = ((OAuth2AuthenticationDetails)details).getTokenValue();
+            //LOGGER.debug("token: {}",accessToken);
+
+            RestTemplate restTemplate = new RestTemplate();
+
+            MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+            params.add("access_token", accessToken);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization", "bearer " + accessToken);
+
+            HttpEntity<String> request = new HttpEntity(params, headers);
+
+            HttpMessageConverter formHttpMessageConverter = new FormHttpMessageConverter();
+            HttpMessageConverter stringHttpMessageConverternew = new StringHttpMessageConverter();
+            restTemplate.setMessageConverters(Arrays.asList(new HttpMessageConverter[]{formHttpMessageConverter, stringHttpMessageConverternew}));
+            try {
+                ResponseEntity<String> response = restTemplate.exchange(logoutUrl, HttpMethod.POST, request, String.class);
+            } catch(HttpClientErrorException e) {
+                //LOGGER.error("HttpClientErrorException invalidating token with SSO authorization server. response.status code: {}, server URL: {}", e.getStatusCode(), logoutUrl);
+            }
+            httpServletRequest.getSession().invalidate();
+            for (Cookie cookie : httpServletRequest.getCookies()) {
+                String cookiePath = httpServletRequest.getContextPath() + "/";
+                cookie.setPath(cookiePath);
+                cookie.setMaxAge(0);
+                httpServletResponse.addCookie(cookie);
+            }
+            //最后调用本地session移除掉
+            super.logout(httpServletRequest,httpServletResponse,authentication);
+        }
+    }
+}
